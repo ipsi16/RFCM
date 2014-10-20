9ba2ae63f57781b90575b7e80fa4ed05e76f4e22
Ipsita Prakash <ipsita16@hotmail.com>
2014-10-21 02:20:54 +0530

    Initial code - Basic rogh set fuzzy clustering code

---
+++ RFCM/src/clustering/rfcm/Cluster.java
@@ -0,0 +1,22 @@
+package clustering.rfcm;
+
+import clustering.rfcm.DataPoint;
+
+public class Cluster
+{
+	public DataPoint centroid;
+	
+	Cluster(DataPoint centroid)
+	{
+		/* this.centroid = centroid;
+		 *  This statement would create a pointer to the input datapoint and will not create a personal copy of the datapoint for this cluster centroid
+		 */
+		this.centroid = new DataPoint();
+		for(int i=0;i<centroid.point.size();i++)
+		{
+			this.centroid.point.add(centroid.point.get(i));
+		}
+	}
+	
+	
+}
